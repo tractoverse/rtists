4e54d87a189de6f6679ab0c85788b797a2d5279e
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2026-05-13 12:24:27 +0200

    Add compatibility with R < 4.3.0. Add rhub config.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,8 @@
 #' @importFrom cli cli_alert_info
 #' @importFrom fiber streamline bundle
+#' 
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
 NULL
 
 .onLoad <- function(libname, pkgname) {
